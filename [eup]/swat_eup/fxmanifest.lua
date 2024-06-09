3529d45ee1dcdeaf874562280683da2e9a593dc7
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-09 07:22:34 -0700

    Afterlyfe-Rp-Dev-Updates

---
+++ [eup]/swat_eup/fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_m_freemode_01_swat.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_swat.meta'
+
+
+author 'Luke M#0237'
+description 'An EUP pack made by Luke M#0237 discord.gg/HYBhcH32cN'
+version '1.0'

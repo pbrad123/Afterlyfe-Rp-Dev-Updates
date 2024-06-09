4a1945b6ecea007ff96657b35de1ce8ca54949fe
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-09 09:28:27 -0700

    [undercover_policecars]

---
+++ [undercover_policecars]/GT2 RS/fxmanifest.lua
@@ -0,0 +1,28 @@
+fx_version 'adamant'
+
+game 'gta5'
+author 'Mzs Modifications'
+description 'GT2 RS 2018'
+
+files {
+
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+	'rover90v8.xml',
+
+}
+
+
+
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'dlctext.meta'
+
+is_els 'true'

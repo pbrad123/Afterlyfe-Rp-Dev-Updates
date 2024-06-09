42cf612254c8b61e4886ea76b3672d2c646d5a7b
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-09 07:30:09 -0700

    [maps]

---
+++ [maps]/brother_hell_wrestling/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Brother'
+description 'Custom MLO'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+files {
+  "interiorproxies.meta"
+}
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+dependency '/assetpacks'

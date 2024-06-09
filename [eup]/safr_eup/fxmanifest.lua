59ac43512c33ddb407874f131a110b0a01cc5e34
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-09 07:20:40 -0700

    Afterlyfe-Rp-Dev-Updates

---
+++ [eup]/safr_eup/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_fbi.meta',
+  'mp_f_freemode_01_mp_f_fbi.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_fbi.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_fbi.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'

52c765542ce5810a89b9b67e6dce4093093db936
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-09 07:24:42 -0700

    [fire]

---
+++ [fire]/MLO_Rewley_Road_Fire_Station/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Generated with AltTool
+
+fx_version 'bodacious'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Blighty 3D'
+description 'Rewley Road'
+
+data_file('DLC_ITYP_REQUEST')('stream/rew_fires_ext.ytyp')
+ 
+files {
+
+	
+}

4bd71b07d57c2fa2591c9b8d5a9091419fa4796d
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-09 09:29:38 -0700

    [voice]

---
+++ [voice]/qb-radio/config.lua
@@ -0,0 +1,89 @@
+Config = {}
+
+Config.RestrictedChannels = {
+    [1] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        doc = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true,
+        sadot = true
+    },
+    [2] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true,
+        sadot = true
+    },
+    [3] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [4] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [5] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [6] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [7] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [8] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [9] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    },
+    [10] = {
+        police = true,
+        sast = true,
+        bcso = true,
+        ambulance = true,
+        firefighter = true,
+        sasp = true
+    }
+}
+
+Config.MaxFrequency = 500

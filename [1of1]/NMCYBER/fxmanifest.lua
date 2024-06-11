7cdcddf27466bfbfe85f856a13204b7e05863e81
pbrad123 <119904266+pbrad123@users.noreply.github.com>
2024-06-10 18:18:37 -0700

    1of1

---
+++ [1of1]/NMCYBER/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Northern Modifications'
+description 'A police Cybertruck by Northern Modifications!'
+version '1.0'
+
+files {
+    'data/*.meta',
+}
+
+data_file 'HANDLING_FILE'			'data/handling.meta'
+data_file 'CARCOLS_FILE'			'data/carcols.meta'
+data_file 'VEHICLE_METADATA_FILE'	'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE'	'data/carvariations.meta'
